03cd1a8d67477fd146b15333ffc9ffb6c99aa308
Sjoerd Scheffer <sjoerd@b00n.org>
2020-11-23 12:46:44 +0100

    Accuracy test for n nearest neighbour

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from sklearn import preprocessing
 from scipy.spatial import distance
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+from operator import attrgetter
+from collections import Counter
 
 import imgreader
 import extraction
@@ -15,7 +17,7 @@ DIR_NAME_TRAIN = os.path.join(".", "data", "train")
 DIR_NAME_TEST = os.path.join(".", "data", "test")
 VISUAL_WORDS_COUNT = 20
 
-
+"""
 def knn(images, tests):
 	num_test = 0
 	correct_predict = 0
@@ -45,9 +47,37 @@ def knn(images, tests):
 			num_test += 1
 			class_based[test_key][1] += 1
 	return [num_test, correct_predict, class_based]
+"""
 
 
-def showResults(visualWordsCount):
+def knn(images, tests, k):
+	num_test = 0
+	correct_predict = 0
+	class_based = {}
+
+	for test_key, test_val in tests.items():
+		class_based[test_key] = [0, 0]  # [correct, all]
+		for tst in test_val:
+			neighbours = []
+			for train_key, train_val in images.items():
+				for train in train_val:
+					neighbours.append({"label": train_key, "distance": distance.euclidean(tst, train)})
+
+			neighbours = sorted(neighbours, key=lambda x: x["distance"])
+			nearestNeighbours = neighbours[slice(k)]
+			nearestNeighbours = [x["label"] for x in nearestNeighbours]
+			counter = Counter(nearestNeighbours)
+			key, occurences = counter.most_common(1)[0]
+
+			if (test_key == key):
+				correct_predict += 1
+				class_based[test_key][0] += 1
+			num_test += 1
+			class_based[test_key][1] += 1
+	return [num_test, correct_predict, class_based]
+
+
+def showResults(visualWordsCount, kRange):
 	print("Loading training set...")
 	trainingCategories = imgreader.getCategories(DIR_NAME_TRAIN)
 
@@ -58,34 +88,44 @@ def showResults(visualWordsCount):
 	(trainingData, testingData) = extraction.getTrainingData(
 		trainingCategories, testCategories, visualWordsCount)
 
-	print("Testing model...")
-	testingData = knn(trainingData, testingData)
+	percentages = []
+	for i in range(1, kRange + 1):
+		print("K = " + str(i) + " Testing model...")
+		testingResults = knn(trainingData, testingData, i)
 
-	print("\nTotal:      " + str(testingData[0]))
-	print("Correct:    " + str(testingData[1]))
-	percentage = (testingData[1] * 100) / (testingData[0] * 100) * 100
-	print("Percentage: " + str(percentage))
+		print("\nTotal:      " + str(testingResults[0]))
+		print("Correct:    " + str(testingResults[1]))
+		percentage = (testingResults[1] * 100) / (testingResults[0] * 100) * 100
+		print("Percentage: " + str(percentage))
 
-	for categoryName, categoryResult in testingData[2].items():
-		print("\nCategory:    " + categoryName)
-		print("Total:       " + str(categoryResult[1]))
-		print("Correct:     " + str(categoryResult[0]))
-		categoryPercentage = (float(categoryResult[0]) * 100) / \
-		                     (float(categoryResult[1]) * 100) * 100
-		print("Percentage:  " + str(categoryPercentage))
+		percentages.append(percentage)
+		"""
+		for categoryName, categoryResult in testingResults[2].items():
+			print("\nCategory:    " + categoryName)
+			print("Total:       " + str(categoryResult[1]))
+			print("Correct:     " + str(categoryResult[0]))
+			categoryPercentage = (float(categoryResult[0]) * 100) / \
+		                     	(float(categoryResult[1]) * 100) * 100
+			print("Percentage:  " + str(categoryPercentage))
+		"""
+	return percentages
 
-	return percentage
 
-
-x = [20, 40, 60, 80, 100, 120, 140, 160, 180, 200]
-y = []
+kRange = 20
+x = [i for i in range(1, kRange + 1)]
+y = showResults(80, kRange)
+# x = [20, 40, 60, 80, 100, 120, 140, 160, 180, 200]
+"""
 for i in range(len(x)):
 	y.append(showResults(x[i]))
+	"""
 
 fig, ax = plt.subplots(1, 1)
-ax.xaxis.set_major_locator(ticker.MultipleLocator(20))
-ax.set_title("Accuracy for different numbers of visual words")
-ax.set_xlabel("Number of visual words")
+ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+# ax.set_title("Accuracy for different numbers of visual words")
+ax.set_title("Accuracy for different numbers of nearest neighbours at 80 visual words")
+# ax.set_xlabel("Number of visual words")
+ax.set_xlabel("Number of nearest neighbours")
 ax.set_ylabel("Accuracy percentage")
 plt.plot(x, y)
 plt.show()
